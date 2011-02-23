42db7fe9eaaed737dae328132494a569fe698ad4
dwenaus <dwenaus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-23 23:26:21 +0000

    2.8.1
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-group-email-subscription/trunk@350119 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-activity-subscription.php
+++ bp-activity-subscription.php
@@ -4,8 +4,8 @@ Plugin Name: BuddyPress Group Email Subscription
 Plugin URI: http://wordpress.org/extend/plugins/buddypress-group-email-subscription/
 Description: Allows group members to receive email notifications for group activity and forum posts instantly or as daily digest or weekly summary.
 Author: Deryk Wenaus, boonebgorges
-Revision Date: February 22, 2011
-Version: 2.8
+Revision Date: February 23, 2011
+Version: 2.8.1
 Requires at least: WPMU 2.9, BuddyPress 1.2
 Tested up to: WP 3.0.5, BuddyPress 1.2.7
 */
