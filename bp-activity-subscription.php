2db4b8672fbdffeb5ec7e4998f0f9a8b4c19a2e8
Boone B Gorges <boonebgorges@gmail.com>
2012-11-26 08:40:43 -0500

    Whitespace

--- bp-activity-subscription.php
+++ bp-activity-subscription.php
@@ -12,7 +12,7 @@ Version: 3.2.1
  * Main loader for the plugin.
  *
  * This function is hooked to bp_include, which is the recommended method for loading BP plugins
- * since BP 1.2.5 or so. When this function is loaded properly, it will unhook 
+ * since BP 1.2.5 or so. When this function is loaded properly, it will unhook
  * activitysub_load_buddypress(). If bp_include is not fired (because you are running a legacy
  * version of BP), the legacy function will load the plugin normally.
  */
@@ -20,7 +20,7 @@ function ass_loader() {
 	if ( bp_is_active( 'groups' ) && bp_is_active( 'activity' ) ) {
 		require_once( dirname( __FILE__ ) . '/bp-activity-subscription-main.php' );
 	}
-	
+
 	remove_action( 'plugins_loaded', 'activitysub_load_buddypress', 11 );
 }
 add_action( 'bp_include', 'ass_loader' );
