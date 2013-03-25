28e7e59bb744fda6f007db8d659122afb5ed63b3
Boone B Gorges <boonebgorges@gmail.com>
2013-03-25 04:06:18 -0400

    Bumps stable tags to 3.3.2

--- bp-activity-subscription.php
+++ bp-activity-subscription.php
@@ -4,8 +4,8 @@ Plugin Name: BuddyPress Group Email Subscription
 Plugin URI: http://wordpress.org/extend/plugins/buddypress-group-email-subscription/
 Description: Allows group members to receive email notifications for group activity and forum posts instantly or as daily digest or weekly summary.
 Author: Deryk Wenaus, boonebgorges, r-a-y
-Revision Date: December 20, 2012
-Version: 3.3.1
+Revision Date: March 25, 2013
+Version: 3.3.3
 */
 
 /**
