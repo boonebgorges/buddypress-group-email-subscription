469a21f4b522b782248e361ac668946221a96b06
r-a-y <r-a-y@gmx.com>
2013-07-24 09:37:48 -0700

    Bump version to 3.4-alpha.

--- bp-activity-subscription.php
+++ bp-activity-subscription.php
@@ -4,8 +4,8 @@ Plugin Name: BuddyPress Group Email Subscription
 Plugin URI: http://wordpress.org/extend/plugins/buddypress-group-email-subscription/
 Description: Allows group members to receive email notifications for group activity and forum posts instantly or as daily digest or weekly summary.
 Author: Deryk Wenaus, boonebgorges, r-a-y
-Revision Date: March 25, 2013
-Version: 3.3.3
+Revision Date: July 24, 2013
+Version: 3.4-alpha
 */
 
 /**
