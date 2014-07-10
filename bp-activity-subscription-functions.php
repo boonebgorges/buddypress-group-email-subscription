f96a2536f13c570350b8dec75ce98426bef2aef2
r-a-y <r-a-y@gmx.com>
2014-07-10 16:53:16 -0700

    Fix notice generated by bbPress when a forum post is submitted.
    bbPress 2.5.4 changed the way emails are sent out and broke a filter that
    GES was using.  bbP 2.5.4 now sends out one email to topic subscribers and
    BCCs everyone that is subscribed to that topic.
    
    For bbPress 2.5.4, this commit filters the list of topic subscribers and
    removes the subscriber from the list if the subscriber is already
    subscribed to the group's "All Mail" option in GES.  This is done to
    prevent duplicate emails.
    
    Fixes #59.

--- bp-activity-subscription-functions.php
+++ bp-activity-subscription-functions.php
@@ -1160,6 +1160,8 @@ function ass_get_default_subscription( $group = false ) {
 //	!TOPIC SUBSCRIPTION
 //
 
+/** BBPRESS *************************************************************/
+
 /**
  * Disables bbPress 2's subscription block if the user's group setting is set
  * to "All Mail".
@@ -1204,16 +1206,110 @@ function ass_bbp_subscriptions( $retval ) {
 add_filter( 'bbp_is_subscriptions_active', 'ass_bbp_subscriptions' );
 
 /**
+ * Stuff to do when the bbPress plugin is ready.
+ *
+ * @since 3.4.1
+ */
+function ass_bbp_ready() {
+	/**
+	 * bbPress v2.5.4 changed how emails are sent out.
+	 *
+	 * They now send one BCC email to their subscribers, so we have to filter out
+	 * the topic subscribers before their email is sent.
+	 */
+	if ( version_compare( bbp_get_version(), '2.5.4' ) >= 0 ) {
+		add_filter( 'bbp_subscription_mail_title', 'ass_bbp_add_topic_subscribers_filter',    99 );
+		add_action( 'bbp_pre_notify_subscribers',  'ass_bbp_remove_topic_subscribers_filter', 0 );
+
+	// bbPress <= v2.5.3
+	} else {
+		add_filter( 'bbp_subscription_mail_message', 'ass_bbp_disable_email', 10, 4 );
+	}
+}
+add_action( 'bbp_ready', 'ass_bbp_ready' );
+
+/**
+ * Removes subscriber from bbP's topic subscriber's list.
+ *
+ * If the recipient is already subscribed to the group's "All Mail" option, we
+ * remove the recipient from bbPress' subscription list to prevent duplicate
+ * emails.
+ *
+ * This scenario might happen if a user subscribed to a bunch of bbP group
+ * topics and later switched to the group's "All Mail" subscription.
+ *
+ * Note: Only applicable for bbPress 2.5.4 and above.
+ *
+ * @since 3.4.1
+ *
+ * @param array $retval Array of user IDs subscribed to a topic
+ * @return array
+ */
+function ass_bbp_remove_topic_subscribers( $retval = array() ) {
+	// make sure we're on a BP group page
+	if ( bp_is_group() ) {
+		global $bp;
+
+		// get group sub status
+		// we're using the direct, global reference for sanity's sake
+		$group_user_subscriptions = groups_get_groupmeta( $bp->groups->current_group->id, 'ass_subscribed_users' );
+
+		// loop through all bbP topic subscribers and check against group sub status
+		foreach ( $retval as $index => $user_id ) {
+			$user_subscription = isset( $group_user_subscriptions[$user_id] ) ? $group_user_subscriptions[$user_id] : false;
+
+			// if user's group status is "All Mail", remove user ID from topic subscribers
+			if ( 'supersub' === $user_subscription ) {
+				unset( $retval[$index] );
+			}
+		}
+	}
+
+	return $retval;
+}
+
+/**
+ * Wrapper function to fire our topic subscriber filter for bbPress.
+ *
+ * This fires when the subscription email subject is being filtered.  Only
+ * applicable for bbPress >= 2.5.4.
+ *
+ * @since 3.4.1
+ *
+ * @param string $retval The subscription email subject
+ * @return string
+ */
+function ass_bbp_add_topic_subscribers_filter( $retval ) {
+	// add our filter to check topic subscribers
+	add_filter( 'bbp_get_topic_subscribers', 'ass_bbp_remove_topic_subscribers' );
+
+	return $retval;
+}
+
+/**
+ * Wrapper function to remove our topic subscriber filter for bbPress.
+ *
+ * This fires before the bbPress subscription email is sent.  Only
+ * applicable for bbPress >= 2.5.4.
+ *
+ * @since 3.4.1
+ */
+function ass_bbp_remove_topic_subscribers_filter() {
+	remove_filter( 'bbp_get_topic_subscribers', 'ass_bbp_remove_topic_subscribers' );
+}
+
+/**
  * Disable bbP's subscription email blast if group user is already subscribed
  * to "All Mail".
  *
  * This scenario might happen if a user subscribed to a bunch of bbP group
  * topics and later switched to the group's "All Mail" subscription.
  *
+ * Note: Only applicable for bbPress 2.5.3 and below.
+ *
  * @since 3.4
  */
 function ass_bbp_disable_email( $message, $reply_id, $topic_id, $user_id ) {
-
 	// make sure we're on a BP group page
 	if ( bp_is_group() ) {
 		global $bp;
@@ -1231,8 +1327,8 @@ function ass_bbp_disable_email( $message, $reply_id, $topic_id, $user_id ) {
 
 	return $message;
 }
-add_filter( 'bbp_subscription_mail_message', 'ass_bbp_disable_email', 10, 4 );
 
+/** LEGACY FORUMS *******************************************************/
 
 function ass_get_topic_subscription_status( $user_id, $topic_id ) {
 	global $bp;
