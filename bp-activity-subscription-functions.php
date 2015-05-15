bf995cbe75d5a63f3df918c41497cef4dc9e6b4b
Jonathan Reeve <jon.reeve@gmail.com>
2015-05-15 13:13:29 -0400

    remove hard-coded references to defaults

--- bp-activity-subscription-functions.php
+++ bp-activity-subscription-functions.php
@@ -1095,7 +1095,7 @@ function ass_default_subscription_settings_form() {
 	<p><?php _e('When new users join this group, their default email notification settings will be:', 'bp-ass'); ?></p>
 	<div class="radio">
 		<label><input type="radio" name="ass-default-subscription" value="no" <?php ass_default_subscription_settings( 'no' ) ?> />
-			<?php _e( 'No Email (users will read this group on the web - good for any group - the default)', 'bp-ass' ) ?></label>
+			<?php _e( 'No Email (users will read this group on the web - good for any group)', 'bp-ass' ) ?></label>
 		<label><input type="radio" name="ass-default-subscription" value="sum" <?php ass_default_subscription_settings( 'sum' ) ?> />
 			<?php _e( 'Weekly Summary Email (the week\'s topics - good for large groups)', 'bp-ass' ) ?></label>
 		<label><input type="radio" name="ass-default-subscription" value="dig" <?php ass_default_subscription_settings( 'dig' ) ?> />
