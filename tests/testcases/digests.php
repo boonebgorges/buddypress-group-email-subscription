18d0dd4c7f87bbaba19bbd804329dacf0a27a6bd
r-a-y <r-a-y@gmx.com>
2016-07-12 00:45:50 -0700

    Unit Tests: Add second group to digest tests.
    Digest tests previously had only one group subscribed to digests.  Add
    a second group so digest tests reflect more of a real-world example.
    
    See #83.

--- tests/testcases/digests.php
+++ tests/testcases/digests.php
@@ -18,24 +18,37 @@ class BPGES_Tests_Digests extends BP_UnitTestCase {
 		parent::tearDown();
 	}
 
+	/**
+	 * @group new-digests
+	 */
 	public function test_new_digests() {
 		$u1 = $this->factory->user->create();
 		$users = $this->factory->user->create_many( 1 );
 
 		$g1 = $this->factory->group->create( array( 'creator_id' => $u1 ) );
+		$g2 = $this->factory->group->create( array( 'creator_id' => $u1 ) );
 
 		// Join users to group and set subscription to daily digest.
 		foreach ( $users as $user ) {
 			groups_join_group( $g1, $user );
 			ass_group_subscription( 'dig', $user, $g1 );
+			ass_group_subscription( 'dig', $user, $g2 );
 		}
 
 		$name = bp_core_get_user_displayname( $u1 );
 
 		// Create a few activity items to add to a user's digest.
-		$a = $this->factory->activity->create_many( 3, array(
+		$this->factory->activity->create_many( 3, array(
 			'type'      => 'activity_update',
 			'item_id'   => $g1,
+			'user_id'   => $u1,
+			'component' => 'groups',
+			'action'    => '<a href="' . bp_core_get_user_domain( $u1 ). '" title="' . $name . '">' . $name . '</a> posted an update in the group <a href="http://localhost/groups/group/">Group</a>',
+		) );
+
+		$this->factory->activity->create_many( 3, array(
+			'type'      => 'activity_update',
+			'item_id'   => $g2,
 			'user_id'   => $u1,
 			'component' => 'groups',
 			'action'    => '<a href="' . bp_core_get_user_domain( $u1 ). '" title="' . $name . '">' . $name . '</a> posted an update in the group <a href="http://localhost/groups/group/">Group</a>',
@@ -45,29 +58,40 @@ class BPGES_Tests_Digests extends BP_UnitTestCase {
 		ass_daily_digest_fire();
 
 		//$mailer = $GLOBALS['ges_mockmailer'];
+		//print_r($mailer);
 	}
 
 	/**
-	 * @group old
+	 * @group old-digests
 	 */
 	public function test_old_digests() {
 		$u1 = $this->factory->user->create();
 		$users = $this->factory->user->create_many( 1 );
 
 		$g1 = $this->factory->group->create( array( 'creator_id' => $u1 ) );
+		$g2 = $this->factory->group->create( array( 'creator_id' => $u1 ) );
 
 		// Join users to group and set subscription to daily digest.
 		foreach ( $users as $user ) {
 			groups_join_group( $g1, $user );
 			ass_group_subscription( 'dig', $user, $g1 );
+			ass_group_subscription( 'dig', $user, $g2 );
 		}
 
 		$name = bp_core_get_user_displayname( $u1 );
 
 		// Create a few activity items to add to a user's digest.
-		$a = $this->factory->activity->create_many( 3, array(
+		$this->factory->activity->create_many( 3, array(
 			'type'      => 'activity_update',
 			'item_id'   => $g1,
+			'user_id'   => $u1,
+			'component' => 'groups',
+			'action'    => '<a href="' . bp_core_get_user_domain( $u1 ). '" title="' . $name . '">' . $name . '</a> posted an update in the group <a href="http://localhost/groups/group/">Group</a>',
+		) );
+
+		$this->factory->activity->create_many( 3, array(
+			'type'      => 'activity_update',
+			'item_id'   => $g2,
 			'user_id'   => $u1,
 			'component' => 'groups',
 			'action'    => '<a href="' . bp_core_get_user_domain( $u1 ). '" title="' . $name . '">' . $name . '</a> posted an update in the group <a href="http://localhost/groups/group/">Group</a>',
@@ -93,19 +117,29 @@ class BPGES_Tests_Digests extends BP_UnitTestCase {
 		$users = $this->factory->user->create_many( 100 );
 
 		$g1 = $this->factory->group->create( array( 'creator_id' => $u1 ) );
+		$g2 = $this->factory->group->create( array( 'creator_id' => $u1 ) );
 
 		// Join users to group and set subscription to daily digest.
 		foreach ( $users as $user ) {
 			groups_join_group( $g1, $user );
 			ass_group_subscription( 'dig', $user, $g1 );
+			ass_group_subscription( 'dig', $user, $g2 );
 		}
 
 		$name = bp_core_get_user_displayname( $u1 );
 
 		// Create a few activity items to add to a user's digest.
-		$a = $this->factory->activity->create_many( 3, array(
+		$this->factory->activity->create_many( 3, array(
 			'type'      => 'activity_update',
 			'item_id'   => $g1,
+			'user_id'   => $u1,
+			'component' => 'groups',
+			'action'    => '<a href="' . bp_core_get_user_domain( $u1 ). '" title="' . $name . '">' . $name . '</a> posted an update in the group <a href="http://localhost/groups/group/">Group</a>',
+		) );
+
+		$this->factory->activity->create_many( 3, array(
+			'type'      => 'activity_update',
+			'item_id'   => $g2,
 			'user_id'   => $u1,
 			'component' => 'groups',
 			'action'    => '<a href="' . bp_core_get_user_domain( $u1 ). '" title="' . $name . '">' . $name . '</a> posted an update in the group <a href="http://localhost/groups/group/">Group</a>',
