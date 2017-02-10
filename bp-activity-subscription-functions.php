afd3160fe09ad81731073c5c0ad1168716c00375
Boone B Gorges <boonebgorges@gmail.com>
2017-02-10 10:27:55 -0600

    More careful array handling in `ass_group_subscription()`.
    Prevents failures in PHP 7.1+.

--- bp-activity-subscription-functions.php
+++ bp-activity-subscription-functions.php
@@ -1091,6 +1091,9 @@ function ass_group_subscription( $action, $user_id, $group_id ) {
 		return false;
 
 	$group_user_subscriptions = groups_get_groupmeta( $group_id , 'ass_subscribed_users' );
+	if ( ! is_array( $group_user_subscriptions ) ) {
+		$group_user_subscriptions = array();
+	}
 
 	// we're being overly careful here
 	if ( $action == 'no' ) {
