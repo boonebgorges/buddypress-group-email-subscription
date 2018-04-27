7a6a61b59658ed858155d6bfdd86ef055148e2b6
Boone B Gorges <boonebgorges@gmail.com>
2018-04-27 13:28:46 -0500

    Use resource clues instead of batch size for firing new processes.
    This is cribbed from `WP_Background_Process`.

--- classes/class-bpges-async-request-send-queue.php
+++ classes/class-bpges-async-request-send-queue.php
@@ -8,10 +8,15 @@ class BPGES_Async_Request_Send_Queue extends WP_Async_Request {
 	protected $action = 'bpges_send_queue';
 
 	/**
-	 * @var   int
+	 * Start time of current process.
+	 *
+	 * (default value: 0)
+	 *
+	 * @var int
+	 * @access protected
 	 * @since 3.9.0
 	 */
-	protected $batch_size;
+	protected $start_time = 0;
 
 	/**
 	 * Migrate a batch of outgoing notifications.
@@ -24,17 +29,9 @@ class BPGES_Async_Request_Send_Queue extends WP_Async_Request {
 			return;
 		}
 
-		$queue_type = wp_unslash( $_POST['type'] );
+		$this->start_time = time();
 
-		/**
-		 * Filters the size of queue batches.
-		 *
-		 * @since 3.9.0
-		 *
-		 * @param int $batch_size Batch size, in number of users.
-		 * @param string $queue_type Type of queue. 'immediate', 'dig', or 'sum'.
-		 */
-		$this->batch_size = apply_filters( 'bpges_send_queue_batch_size', 2, $queue_type );
+		$queue_type = wp_unslash( $_POST['type'] );
 
 		switch ( $queue_type ) {
 			case 'immediate' :
@@ -50,27 +47,114 @@ class BPGES_Async_Request_Send_Queue extends WP_Async_Request {
 	}
 
 	protected function handle_immediate_queue( $activity_id ) {
-		$query = new BPGES_Queued_Item_Query( array(
-			'activity_id' => $activity_id,
-			'per_page'    => $this->batch_size,
-		) );
 
-		$items = $query->get_results();
+		$run = true;
+		$i = 1;
+		do {
+			$num_queries = $GLOBALS['wpdb']->num_queries;
+			$i++;
+			$query = new BPGES_Queued_Item_Query( array(
+				'activity_id' => $activity_id,
+				'per_page'    => 1,
+			) );
 
-		// Queue is finished.
-		if ( ! $items ) {
-			// @todo Should we take this opportunity to run a cleanup of other failed 'immediate' items? or maybe on a cron job
-			return;
-		}
+			$items = $query->get_results();
 
-		foreach ( $items as $item ) {
-			bpges_generate_notification( $item );
-			$item->delete();
-		}
+			// Queue is finished.
+			if ( ! $items ) {
+				// @todo Should we take this opportunity to run a cleanup of other failed 'immediate' items? or maybe on a cron job
+				return;
+			}
+
+			foreach ( $items as $item ) {
+				bpges_generate_notification( $item );
+				$item->delete();
+			}
+
+			if ( $this->time_exceeded() || $this->memory_exceeded() ) {
+				$run = false;
+			}
+		} while ( $run );
 
 		bpges_send_queue()->data( array(
 			'type'        => 'immediate',
 			'activity_id' => $activity_id,
 		) )->dispatch();
+	}
+
+	/**
+	 * Memory exceeded
+	 *
+	 * Ensures the batch process never exceeds 90%
+	 * of the maximum WordPress memory.
+	 *
+	 * Copied from WP_Background_Process.
+	 *
+	 * @return bool
+	 */
+	protected function memory_exceeded() {
+		$memory_limit   = $this->get_memory_limit() * 0.9; // 90% of max memory
+		$current_memory = memory_get_usage( true );
+		$return         = false;
+
+		if ( $current_memory >= $memory_limit ) {
+			$return = true;
+		}
+
+		return apply_filters( $this->identifier . '_memory_exceeded', $return );
+	}
+
+	/**
+	 * Get memory limit
+	 *
+	 * Copied from WP_Background_Process.
+	 *
+	 * @return int
+	 */
+	protected function get_memory_limit() {
+		if ( function_exists( 'ini_get' ) ) {
+			$memory_limit = ini_get( 'memory_limit' );
+		} else {
+			// Sensible default.
+			$memory_limit = '128M';
+		}
+
+		if ( ! $memory_limit || -1 === intval( $memory_limit ) ) {
+			// Unlimited, set to 32GB.
+			$memory_limit = '32000M';
+		}
+
+		return intval( $memory_limit ) * 1024 * 1024;
+	}
+
+	/**
+	 * Time exceeded.
+	 *
+	 * Ensures the batch never exceeds a sensible time limit.
+	 * A timeout limit of 30s is common on shared hosting.
+	 *
+	 * Copied mostly from WP_Background_Process.
+	 *
+	 * @return bool
+	 */
+	protected function time_exceeded() {
+		if ( function_exists( 'ini_get' ) ) {
+			$max_execution_time = ini_get( 'max_execution_time' );
+		} else {
+			// Sensible default.
+			$max_execution_time = '30';
+		}
+
+		$finish = $this->start_time + $max_execution_time;
+		$return = false;
+
+		$time = time();
+
+		// Don't get within 10 seconds of max time.
+		if ( $time >= ( $finish - 10 ) ) {
+			$return = true;
+		}
+
+		return apply_filters( $this->identifier . '_time_exceeded', $return );
 	}
 }
