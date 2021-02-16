f9530601593f90115432a91ba4eefe6f3245dd20
Boone B Gorges <boonebgorges@gmail.com>
2021-02-16 11:07:12 -0600

    Swap out deprecated jQuery method.

--- bp-activity-subscription-js.js
+++ bp-activity-subscription-js.js
@@ -3,7 +3,7 @@ jQuery(document).ready( function($) {
 
 	if ( groupRow.find( 'div.ges-panel' ).length ) {
 		repositionGESPanel();
-		$( window ).resize(function() {
+		$( window ).on('resize', function() {
 			repositionGESPanel();
 		});
 	}
