2c9ffa34807aa0ecea67e9775b69e12f4cb6bb8a
Boone B Gorges <boonebgorges@gmail.com>
2024-07-24 14:56:58 -0500

    PHPCS for ges-email-notice.php.

--- templates/groups/single/admin/ges-email-notice.php
+++ templates/groups/single/admin/ges-email-notice.php
@@ -7,21 +7,21 @@
  */
 ?>
 
-<h3><?php _e( 'Send an email notice to everyone in the group', 'buddypress-group-email-subscription' ); ?></h3>
-<p><?php _e( 'You can use the form below to send an email notice to all group members.', 'buddypress-group-email-subscription' ); ?>
+<h3><?php esc_html_e( 'Send an email notice to everyone in the group', 'buddypress-group-email-subscription' ); ?></h3>
+<p><?php esc_html_e( 'You can use the form below to send an email notice to all group members.', 'buddypress-group-email-subscription' ); ?>
 
 <?php if ( bpges_force_immediate_admin_notice() ) : ?>
 	<br>
-	<b><?php _e( 'Everyone in the group will receive the email -- regardless of their email settings -- so use with caution', 'buddypress-group-email-subscription' ); ?></b>.</p>
+	<b><?php esc_html_e( 'Everyone in the group will receive the email -- regardless of their email settings -- so use with caution', 'buddypress-group-email-subscription' ); ?></b>.</p>
 <?php endif; ?>
 
 <p>
-	<label for="ass-admin-notice-subject"><?php _e( 'Email Subject:', 'buddypress-group-email-subscription' ) ?></label>
+	<label for="ass-admin-notice-subject"><?php esc_html_e( 'Email Subject:', 'buddypress-group-email-subscription' ); ?></label>
 	<input type="text" name="ass_admin_notice_subject" id="ass-admin-notice-subject" value="" />
 </p>
 
 <p>
-	<label for="ass-admin-notice-textarea"><?php _e( 'Email Content:', 'buddypress-group-email-subscription' ) ?></label>
+	<label for="ass-admin-notice-textarea"><?php esc_html_e( 'Email Content:', 'buddypress-group-email-subscription' ); ?></label>
 	<textarea value="" name="ass_admin_notice" id="ass-admin-notice-textarea"></textarea>
 </p>
 
